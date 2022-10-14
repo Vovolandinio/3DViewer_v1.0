b932c3c10b458ac9abc57da2e408b3fabf603807
warbirdo <warbirdo@student.21-school.ru>
2022-10-14 17:32:38 +0300

    First version of parsing index V. Need test.

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -13,6 +13,8 @@ void main_parser(const char* filename, index* src) {
     if ((file = fopen(filename, "r")) == NULL){
         printf("Cannot open file");
     } else {
+        src->polygon->vertexes = (int*)calloc(1, sizeof(int));
+        src->vertexes = (unsigned*)calloc(1, sizeof(unsigned));
         char buffer = '0';
         while(notEOF) {
             char c = fgetc(file);
@@ -27,7 +29,16 @@ void main_parser(const char* filename, index* src) {
 
 
 void parser_v(FILE *file, index* src){
-
+    if(src->vertexes != NULL) {
+        src->vertexes = (float*)realloc(src->array, (src->indexV + 3) * sizeof(float));
+        for (size_t i = 0; i < 0; i++) {
+            src->vertexes[src->indexV + i];
+        }
+        fscanf(file, "%f %f %f", &src->vertexes[src->indexV], &src->vertexes[src->indexV + 1], &src->vertexes[src->indexV + 2]);
+    src->indexV += 3;
+    } else {
+        printf("Memory error");
+    }
 }
 
 void parser_f(FILE *file, index* src){
