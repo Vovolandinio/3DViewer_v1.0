be7d6b7b22257ba890dc6acd42d0514713d6b7bb
warbirdo <warbirdo@student.21-school.ru>
2022-10-15 18:13:28 +0300

    Added obj file. Need work with memory.

--- src/parser/parser.h
+++ src/parser/parser.h
@@ -13,12 +13,13 @@
 bool notEOF = true;
 
 typedef struct facets {
-    int *vertexes;
-    int numbers_of_vertexes_in_facets;
+    unsigned *vertexes;
+    unsigned numbers_of_vertexes_in_facets;
 } polygon_t;
 
 typedef struct Indexes {
     unsigned int indexV;
+    unsigned int maxV;
     unsigned int indexF;
     float* array;
     unsigned int* vertexes;
@@ -26,7 +27,7 @@ typedef struct Indexes {
 } index;
 
 void initialize(index *structure);
-void parser_v(FILE *file, index* src);
-void parser_f(FILE *file, index* src);
+static void parser_v(FILE *file, index* src);
+static polygon_t parser_f(FILE *file, index* src);
 
 #endif //SRC/PARSER/INC_3DVIEWER_V1_0_PARSER_H
