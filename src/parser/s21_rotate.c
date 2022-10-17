109cb4307a1dc0fae1891b5906b32b4d4484bf88
vladimir <@warbirdo_test_frontent_osnova>
2022-10-17 22:54:03 +0300

    Project structure refactoring. Added affine transformations (s21_move.c s21_rotate.c). Need tests refactoring the main parser.

---
+++ src/parser/s21_rotate.c
@@ -0,0 +1,33 @@
+#include "s21_parser.h"
+
+void rotate_x(indexes *src, double x) {
+    for(size_t i = 0;i < src->indexV; i+=3) {
+        double array = src->array[i];
+        src->array[i] *= cos(x);
+        src->array[i] += src->array[i + 1] * sin(x);
+        src->array[i + 1] *= cos(x);
+        src->array[i + 1] -= array * sin(x);
+    }
+}
+
+void rotate_y(indexes *src, double x) {
+    for(size_t i = 0;i < src->indexV; i+=3) {
+        double array = src->array[i];
+        src->array[i] *= cos(x);
+        src->array[i] -= src->array[i + 2] * sin(x);
+        src->array[i + 2] *= cos(x);
+        src->array[i + 2] += x * sin(x);
+    }
+}
+
+void rotate_z(indexes *src, double x) {
+    for(size_t i = 0;i < src->indexV; i+=3) {
+        double array = src->array[i];
+        src->array[i] *= cos(x);
+        src->array[i] += src->array[k + 1] * sin(x);
+        src->array[i + 1] *= cos(x);
+        src->array[i + 1] -= x * sin(x);
+    }
+}
+
+
