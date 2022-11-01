d9fd6ea0bc6eb4064242399abe6c58f1105f3cc1
warbirdo <warbirdo@student.21-school.ru>
2022-11-01 16:56:40 +0300

    fixed rotate

--- src/parser/s21_rotate.c
+++ src/parser/s21_rotate.c
@@ -1,34 +1,37 @@
 #include "s21_parser.h"
 
 void rotate_x(float* array, int indexV, double x) {
-    x = x * M_PI / 180;
-    for (size_t i = 1; i < indexV; i = i + 3) {
-        double buff_array = array[i];
-        array[i] *= cos(x);
-        array[i] += array[i + 1] * sin(x);
-        array[i + 1] *= cos(x);
-        array[i + 1] -= buff_array * sin(x);
+    if (x != 0){
+    double agle = x * (M_PI / 180);
+    for (unsigned int i = 0; i < indexV; i = i + 3) {
+        double buff_array_y = array[i + 1];
+        double buff_array_z = array[i + 2];
+        array[i + 1] = cos(agle) * buff_array_y - sin(agle) * buff_array_z;
+        array[i + 2] = sin(agle) * buff_array_y + cos(agle) * buff_array_z;
+    }
     }
 }
 
-void rotate_y(float* array, int indexV, double x) {
-    x = x * M_PI / 180;
-    for (size_t i = 0; i < indexV; i = i + 3) {
-        double buff_array = array[i];
-        array[i] *= cos(x);
-        array[i] -= array[i + 2] * sin(x);
-        array[i + 2] *= cos(x);
-        array[i + 2] += buff_array * sin(x);
+void rotate_y(float* array, int indexV, double y) {
+    if (y != 0) {
+    double agle = y * (M_PI / 180);
+    for (unsigned int i = 0; i < indexV; i = i + 3) {
+        double buff_array_x = array[i];
+        double buff_array_z = array[i + 2];
+        array[i] = cos(agle) * buff_array_x - sin(agle) * buff_array_z;
+        array[i + 2] = sin(agle) * buff_array_x + cos(agle) * buff_array_z;
+    }
     }
 }
 
-void rotate_z(float* array, int indexV, double x) {
-    x = x * M_PI / 180;
-    for (size_t i = 0; i < indexV; i = i + 3) {
-        double buff_array = array[i];
-        array[i] *= cos(x);
-        array[i] += array[i + 1] * sin(x);
-        array[i + 1] *= cos(x);
-        array[i + 1] -= buff_array * sin(x);
+void rotate_z(float* array, int indexV, double z) {
+    if (z != 0) {
+    double agle = z * (M_PI / 180);
+    for (unsigned int i = 0; i < indexV; i = i + 3) {
+        double buff_array_x = array[i];
+        double buff_array_y = array[i + 1];
+        array[i] = cos(agle) * buff_array_x - sin(agle) * buff_array_y;
+        array[i + 1] = sin(agle) * buff_array_x + cos(agle) * buff_array_y;
+    }
     }
 }
