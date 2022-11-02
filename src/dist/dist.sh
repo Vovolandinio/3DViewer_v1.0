b251246a06ccd07961d481aeee18ffee89407426
warbirdo <warbirdo@student.21-school.ru>
2022-11-02 19:21:51 +0300

    Update dist.sh script and Makefile.

--- src/dist/dist.sh
+++ src/dist/dist.sh
@@ -2,12 +2,10 @@
 
 cd qtViewer
 mkdir 3D_viewer_dist
-cp *.c 3D_viewer_dist
 cp *.h 3D_viewer_dist
-cp qtViewer.pro.user 3D_viewer_dist
 cp *.pro 3D_viewer_dist
 cp *.cpp 3D_viewer_dist
 cp *.ui 3D_viewer_dist
-cp *.sh 3D_viewer_dist
-cp clang-format 3D_viewer_dist
 tar -czf 3D_viewer_dist.tar 3D_viewer_dist/
+mv 3D_viewer_dist.tar ../dist
+mv 3D_viewer_dist ../dist
